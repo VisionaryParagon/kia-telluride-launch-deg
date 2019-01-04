413988050c091eac16bbaaf4888e2e97e61285de
stephenautomatik <sdickens@automatik.us>
2019-01-03 22:32:16 -0700

    QR code scanner URL validation added

--- src/app/main/switched-on/qr-scanner/qr-scanner.component.ts
+++ src/app/main/switched-on/qr-scanner/qr-scanner.component.ts
@@ -57,6 +57,18 @@ export class QrScannerComponent implements OnInit {
 
   handleQrCodeResult(ev) {
     // alert('Scan: ' + ev);
-    window.location = ev;
+    const urlString = new RegExp('^(https?:\\/\\/)' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name and extension
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?' + // port
+      '(\\/[-a-z\\d%@_.~+&:]*)*' + // path
+      '(\\?[;&a-z\\d%@_.,~+&:=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+
+    if (urlString.test(ev)) {
+      window.location = ev;
+    } else {
+      alert('This QR code scanner only accepts codes with a valid URL. Please scan another code.');
+    }
   }
 }
