82303addf6c1bf5c10d76e727c3eb3f2adf19372
stephenautomatik <sdickens@automatik.us>
2019-01-03 19:28:11 -0700

    QR scanner removed from pre-game component

--- src/app/main/pre-game/pre-game.component.ts
+++ src/app/main/pre-game/pre-game.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
 
 import { FadeAnimation, TopDownAnimation } from '../../animations';
-
-import { ScannerComponent } from '../modals/scanner/scanner.component';
 
 @Component({
   selector: 'app-pre-game',
@@ -13,25 +10,8 @@ import { ScannerComponent } from '../modals/scanner/scanner.component';
 })
 export class PreGameComponent implements OnInit {
 
-  constructor(
-    public dialog: MatDialog
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  openScanner() {
-    const dialogRef = this.dialog.open(ScannerComponent, {
-      height: '90vh',
-      maxWidth: '90vw',
-      width: '90vw'
-    });
-
-    dialogRef.afterClosed()
-      .subscribe(
-        data => {
-          // console.log('Updated user:', data);
-        }
-      );
   }
 }
